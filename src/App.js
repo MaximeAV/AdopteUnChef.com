bbdf53f6e1b149a67d2da6359354de50ad22cf9a
Maxime <maxime.avaulee@gmail.com>
2021-03-04 14:36:19 +0100

    Setup project

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eh Paul ?
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.dailymotion.com/video/x67m4h"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ta gueule !
         </a>
       </header>
     </div>
