8ae1b44422f740bef6cf733f19b219635099ed8d
Maxime <maxime.avaulee@gmail.com>
2021-03-06 19:38:39 +0100

    Merge front test

--- src/front/src/index.js
+++ src/front/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Signin from './signin/signin'
@@ -9,19 +8,6 @@ import Signin from './signin/signin'
 ReactDOM.render(
   <React.StrictMode>
     <Signin />
-=======
-import reportWebVitals from './reportWebVitals';
-import Navigator from './Navigator'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Navigator />
->>>>>>> register
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
