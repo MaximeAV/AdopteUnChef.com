e4c70cc705075871f415c8f0fd271f0adc472234
Maxime <maxime.avaulee@gmail.com>
2021-03-08 20:13:57 +0100

    Update server.js

---
+++ src/back/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+const bodyparser=require('body-parser');
+
+/* const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./src/Swagger/swagger.yaml'); */
+app.use(bodyparser.json());
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Authorization, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
+});
+
+var databaseRouter = require('./routes/databaseRouter');
+var port = 4000;
+
+app.use('/api/db', databaseRouter); 
+
+//app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, function () {
+  console.log('The API is listening on port ' + port);
+});
