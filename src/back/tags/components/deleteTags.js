55462f61a9043dedb028d5cbb8e0eb921496507c
Maxime <maxime.avaulee@gmail.com>
2021-03-10 19:56:50 +0100

    Update swagger

--- src/back/tags/components/deleteTags.js
+++ src/back/tags/components/deleteTags.js
@@ -2,11 +2,11 @@ var db = require('../../Database/database');
 
 async function getDelete(body) {
   result = {};
-  let id = body.id;
-  var res = await db('tags').where({ id: id }).del();
+  let tag = body.tag;
+  var res = await db('tags').where({ tag: tag }).del();
   if (res != 0) {
     result['msg'] = 'Successfully deleted';
-    console.log('Deleted tag ' + id);
+    console.log('Deleted tag ' + tag);
   } else {
     result['msg'] = 'Nothing to delete';
     console.log('Nothing to delete');
