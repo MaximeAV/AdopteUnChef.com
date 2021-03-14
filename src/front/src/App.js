7dbd9d50fd012573315cb914650a33db7c9c4981
Yoann Lefebvre <lefebvre.yoann@outlook.fr>
2021-03-14 03:51:46 +0100

    get publications done

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -25,6 +25,41 @@ function App() {
     }
   ])
 
+  const handlePublications = (event) => {
+    event.preventDefault();
+    console.log('Get publications from database...')
+    
+    // POST request using fetch with error handling
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Authorization', 'Basic ');
+    headers.append('Origin','http://localhost:3000');
+    
+    const requestOptions = {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+        headers: headers,
+    };
+    fetch('http://localhost:4000/api/db/publications/', requestOptions)
+        .then(async res => {
+            const data = await res.json();
+            console.log(data);
+            setPublications(data.reverse());
+            // check for error response
+            if (!res.ok) {
+                // get error message from body or default to response status
+                const error = (data && data.message) || res.status;
+                return Promise.reject(error);
+            }
+        })
+        .catch(error => {
+            console.error('There was an error!', error);
+        });
+  }
+
   return (
     <div className="app">
 
@@ -34,9 +69,11 @@ function App() {
           <h1>AdopteUnChef.com</h1>
         </div>
 
+        <button onClick={handlePublications}>Charger publis</button>
+
         {
           publications.map(publication =>(
-            <Publication username={publication.username} description={publication.description} image={publication.image}/>
+            <Publication username="Username" description={publication.description} image={publication.image}/>
           ))
         }
         
